44268139af3af83b9204f6db245599d32944484f
AlekseySelivanov <salex92@mail.ru>
2020-10-19 08:49:17 +0300

    ДЗ 4

---
+++ src/ru/geekbrains/homework/ChatServer.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.homework;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server stared at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
